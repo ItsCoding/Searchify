5b53313370b3c5d305638cd91a83e430e3921144
Alexander Quadt <10713197+ItsCoding@users.noreply.github.com>
2022-05-23 14:26:15 +0200

    Adding Github link to footer

--- src/App.tsx
+++ src/App.tsx
@@ -65,19 +65,20 @@ function App() {
         <>
           <Header />
           <Engine token={token} />
-          <div
-            style={{
-              textAlign: "center",
-              paddingTop: 15,
-            }}
-          >
-            <p>Coded with ❤</p> <small style={{ color: "#545454" }}>
-              metadata provided by <FontAwesomeIcon icon={faSpotify} size={"lg"} />
-              {/* <img style={{ height: 15, marginLeft: 5 }} src="Spotify_Logo_CMYK_White.png"></img> */}
-            </small>
-          </div>
+
         </>
       )}
+      <div
+        style={{
+          textAlign: "center",
+          paddingTop: 15,
+        }}
+      >
+        <p>Coded with ❤ | See on <a href="https://github.com/ItsCoding/Searchify" rel="noreferrer" target="_blank" >Github</a></p> <small style={{ color: "#545454" }}>
+          metadata provided by <FontAwesomeIcon icon={faSpotify} size={"lg"} />
+          {/* <img style={{ height: 15, marginLeft: 5 }} src="Spotify_Logo_CMYK_White.png"></img> */}
+        </small>
+      </div>
     </>
   );
 }
